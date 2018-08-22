c2d75ebe4e7a0c692b6c2a6ceaaf3fe77ddaa281
kevin olson <acidjazz@gmail.com>
2018-08-22 17:55:32 -0500

    ✨ solid attempt at a global programmatic component

---
+++ web/components/global/Modal/Modal.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+transition(name="animodal")
+  .modal.is-active(v-if="isActive")
+    .modal-background
+    .modal-card
+      .modal-card-head
+        p.modal-card-title Check your Inbox
+        button.delete(@click="destroy")
+      section.modal-card-body
+        p An e-mail was sent to your account with a link to login.
+        hr
+        p You may 
+          b close this window/tab now
+          |, since clicking the link will open a new one!
+        hr
+        p {{ message }}
+      footer.modal-card-foot.justify-flex-end
+        .buttons.is-right
+          .button.button OK
+</template>
+
+<script>
+export default {
+  name: 'GlobalModal',
+  props: {
+    message: String
+  },
+  methods: {
+    destroy() {
+      if (typeof (this.$el).remove !== 'undefined') {
+        this.$el.remove()
+      } else {
+        this.$el.parentNode.removeChild(el)
+      }
+    },
+  },
+
+  mounted () {
+    console.log(this.message)
+    this.isActive = true
+  },
+  data () {
+    return {
+      isActive: false,
+    }
+  }
+}
+</script>
+
