943e5e1bfb553fed01bfce453afceda37329e28e
kevin olson <acidjazz@gmail.com>
2018-08-31 11:16:07 -0500

    🎨 more transition tweaking

--- web/components/layout/navbar/NavBarUser.vue
+++ web/components/layout/navbar/NavBarUser.vue
@@ -1,23 +1,33 @@
 <template lang="pug">
 .navbar-end
-  .navbar-item.has-dropdown.is-hoverable 
-    .navbar-link
+  .navbar-item.has-dropdown(:class="{'is-active': active}")
+    a.navbar-link(@click="active = !active")
       .level
         .level-left
           .level-item
-            figure.image.is-32x32
+            figure.image.is-24x24
               img.is-rounded(:src="$store.state.user.avatar")
           .level-item
             span {{ $store.state.user.name }}
     .navbar-dropdown.is-right
       hr.navbar-divider
-      NavBarLogout
+      NavBarLogout(:close="close")
 </template>
 
 <script>
 import NavBarLogout from '@/components/layout/navbar/NavBarLogout'
 export default {
-  components: { NavBarLogout }
+  components: { NavBarLogout },
+  methods: {
+    close () {
+      this.active = false
+    },
+  },
+  data () {
+    return {
+      active: false,
+    }
+  },
 }
 </script>
 
