acd3836ab18fa8766e7a28f8e4b8a1358ad6c4b2
kevin olson <acidjazz@gmail.com>
2018-09-02 03:38:12 -0500

    🚧 adding storage folder and latest style guide

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -1,27 +1,93 @@
 <template lang="pug">
 #Home.page
-  .container
+    section.hero.is-primary
+      .container
+        .hero-body
+          .container
+            h1.title trendjet style guide
+            h2.subtitle colors, fonts, and components
     section.section
       .content
-        // code {{ cookies }}
-      .buttons.is-centered
+        h2 Colors
+        hr.divider.has-background-primary
+        .box
+          .columns.is-multiline
+            .column(v-for="hex, name in colors")
+              .notification(
+                :style="`background-color: ${hex};`"
+                :class="{'has-text-white': light.indexOf(name) != -1}")
+                p {{ name }}
+                p {{ hex }}
+
+        h2 Fonts
+        hr.divider.has-background-primary
+        .box
+          p trendjet uses Google's 
+            a(href="https://fonts.google.com/specimen/Roboto") roboto 
+            | font
+            ul
+              li(v-for="n in 7",:class="`is-size-${n}`") size {{ n }}
+        h2 Global Programmatic Components
+        hr.divider.has-background-primary
+
+        h3 Modal
+        pre: code.language-js.
+          this.$modal.show('this is a simple modal')
         .button(@click="modal") show modal
-        .button(@click="message") show message
-        .button(@click="loginas") login as acidjazz@gmail.com
+
+        h3 Detailed Modal
+        pre: code.language-js.
+          this.$modal.show({
+            title: 'title', 
+            body: 'this is the body',
+            buttons: [
+              {name: 'OK', class: 'is-primary'},
+              {name: 'Cancel'},
+            ]})
+        .button(@click="modalDetailed") show detailed modal
+
+        h3 Toast
+        pre: code.language-js.
+          this.$toast.show('this is a toast')
+        .button(@click="toast") show toast
+
+        h3 Detailed Toast
+        pre: code.language-js.
+          let types = ['default','success','info','danger']
+          let type = types[Math.floor(Math.random() * types.length)]
+          this.$toast.show({
+            type: type, 
+            message: `this toast is type ${type}`})
+        .button(@click="toastDetailed") show toast
+
+
 </template>
 
 <script>
 export default {
   methods: {
     modal () {
-      this.$modal.show({title: 'testing', body: 'this is a test', buttons: [{name: 'OK'}] })
+      this.$modal.show('this is a simple modal')
+    },
+    modalDetailed () {
+      this.$modal.show({
+        title: 'title', 
+        body: 'this is the body',
+        buttons: [
+          {name: 'OK', class: 'is-primary'},
+          {name: 'Cancel'},
+        ]})
     },
 
-    message () {
-      this.$message.show({
-        type: this.types[Math.floor(Math.random()*this.types.length)], 
-        message: `this is a test message`}
-      )
+    toast () {
+      this.$toast.show('this is a toast')
+    },
+    toastDetailed () {
+      let types = ['default','success','info','danger']
+      let type = types[Math.floor(Math.random() * types.length)]
+      this.$toast.show({
+        type: type, 
+        message: `this toast is type ${type}`})
     },
 
     async loginas () {
@@ -41,6 +107,29 @@ export default {
       cookies: {},
       counter: 0,
       types: [ 'success', 'info', 'warning', 'danger' ],
+      colors: {
+        picton: '#01b6fa',
+        malibu: '#01c2f9',
+        sky: '#60d4fd',
+        sail: '#a3e3fd',
+        pattens: '#d9f2fe',
+        squeeze: '#F4F7FA',
+        romance: '#fefefe',
+        zircon: '#DBE0E8',
+      },
+      light: [ 'picton','malibu' ],
+    }
+  },
+
+  head () {
+    return {
+      link: [
+        { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.15.0/themes/prism.min.css' }, 
+        { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.15.0/themes/prism-okaidia.min.css' },
+      ],
+      script: [
+        { src: 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.15.0/prism.min.js' },
+      ]
     }
   },
 }
