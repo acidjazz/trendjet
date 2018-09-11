8f74803743b1ab9303143969aef09c32b73db456
kevin olson <acidjazz@gmail.com>
2018-09-10 21:14:23 -0500

    🔧 progress

--- web/components/layout/navbar/NavBarUser.vue
+++ web/components/layout/navbar/NavBarUser.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 .navbar-end
-  .navbar-item.has-dropdown.is-hoverable
-    .navbar-link
+  .navbar-item.has-dropdown(:class="{'is-active': active }",v-on-clickaway="off")
+    .navbar-link(@click="swap")
       .level
         .level-left
           .level-item
@@ -9,13 +9,18 @@
               img.avatar.object-cover.ani-zoom-in(:src="$store.state.user.avatar")
           .level-item
             span {{ $store.state.user.name }}
-    .navbar-dropdown.is-right
-      nuxt-link.navbar-item(v-if="isAdmin",to="/admin",@click.native="close")
+    .navbar-dropdown.is-right(:class="{'is-closing': closing}")
+
+      nuxt-link.navbar-item(to="/videos",@click.native="off")
+        span.icon
+          i.mdi.mdi-youtube
+        span My Videos
+ 
+      nuxt-link.navbar-item(v-if="isAdmin",to="/admin",@click.native="off")
         span.icon
           i.mdi.mdi-security
         span Admin
-      hr.navbar-divider
-      nuxt-link.navbar-item(to="/guide",@click.native="close")
+      nuxt-link.navbar-item(to="/guide",@click.native="off")
         span.icon
           i.mdi.mdi-palette-swatch
         span Style Guide
@@ -26,13 +31,41 @@
 <script>
 import NavBarLogout from '@/components/layout/navbar/NavBarLogout'
 import { mapGetters } from 'vuex'
+import active from '@/mixins/active'
+import { mixin as clickaway } from 'vue-clickaway'
 export default {
   computed: { ...mapGetters(['isAdmin']), },
   components: { NavBarLogout },
+  mixins: [ clickaway ],
   props: {
     close: {
       type: Function,
       required: true,
+    }
+  },
+
+  methods: {
+    swap () {
+      if (this.active) {
+        return this.off()
+      }
+      return this.on()
+    },
+    on () { this.active = true },
+    off () {
+      this.closing = true
+      setTimeout(() => {
+        this.closing = false
+        this.active = false
+        this.close()
+      }, 400)
+    }
+  },
+
+  data () {
+    return {
+      closing :false,
+      active: false,
     }
   }
 }
---
+++ web/pages/videos.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+#Videos.page
+  section.section
+    .container
+      .level
+        .level-left
+          .level-item 16 Videos
+        .level-right
+          .level-item
+            AddVideo
+</template>
+
+<script>
+import AddVideo from '@/components/video/AddVideo'
+export default {
+  components: { AddVideo }
+}
+</script>
