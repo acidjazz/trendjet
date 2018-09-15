65b1b0a7e325549de13f36158aec3d0eb05f97a9
kevin olson <acidjazz@gmail.com>
2018-09-15 18:19:11 -0500

    ✨ more video browsing progress

---
+++ web/components/video/VideoCardLoading.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+.card
+  .card-image.is-loading
+    figure.image.is-16by9
+  .card-content
+    .field.is-grouped.is-grouped-multiline
+      .control
+        .tags.has-addons
+          span.tag.is-dark Views
+          span.tag.is-info.is-loading &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      .control
+        .tags.has-addons
+          span.tag.is-dark Added
+          span.tag.is-info.is-loading &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  footer.card-footer
+    .card-footer-item.has-text-grey
+      span.icon
+        i.mdi.mdi-history
+      span Details
+    .card-footer-item.has-text-grey
+      span.icon
+        i.mdi.mdi-rocket
+      span Boost 
+    .card-footer-item.has-text-grey
+      span.icon
+        i.mdi.mdi-delete
+      span Delete 
+
+</template>
+
+<script>
+export default {
+}
+</script>
--- web/components/video/VideoList.vue
+++ web/components/video/VideoList.vue
@@ -1,13 +1,14 @@
 <template lang="pug">
 .columns.is-multiline
-  .column(v-for="video in videos").is-one-third.ani-zoom-in
+  .column(v-for="video in videos").is-one-third.ani-zoom-in-half
     VideoCard(:video="video",:channel="channel")
 </template>
 
 <script>
+import VideoCardLoading from '@/components/video/VideoCardLoading'
 import VideoCard from '@/components/video/VideoCard'
 export default {
-  components: { VideoCard },
+  components: { VideoCard, VideoCardLoading },
   props: {
     videos: {
       type: [Object,Array],
