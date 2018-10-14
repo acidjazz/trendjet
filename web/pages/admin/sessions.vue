119af49f1b4de7aaa0ec443d28694381975f652a
kevin olson <acidjazz@gmail.com>
2018-10-13 22:53:26 -0500

    ✨ working loginAs functionality

--- web/pages/admin/sessions.vue
+++ web/pages/admin/sessions.vue
@@ -1,8 +1,13 @@
 <template lang="pug">
+#Sessions.page-admin
+  .container
+    ConnectedDevices(:admin="true")
 </template>
 
 <script>
+import ConnectedDevices from '@/components/pages/profile/ConnectedDevices'
 export default {
   middleware: ['is-admin'],
+  components: { ConnectedDevices },
 }
 </script>
