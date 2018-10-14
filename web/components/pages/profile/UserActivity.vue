8eae897c7d180dd02b59d3ef954288a002c1e6f2
kevin olson <acidjazz@gmail.com>
2018-10-13 20:50:39 -0500

    🎨 copy and map link

--- web/components/pages/profile/UserActivity.vue
+++ web/components/pages/profile/UserActivity.vue
@@ -34,7 +34,14 @@
             i.mdi.mdi-apple-safari(v-if="session.device.browser.includes('Safari')")
             i.mdi.mdi-internet-explorer(v-if="session.device.browser.includes('Internet Explorer')")
         td
-          div {{ session.location.city }}, {{ session.location.state }}
+          span {{ session.location.city }}, {{ session.location.state }}
+          span &nbsp;
+          a.button.is-small(
+            :href="`https://www.google.com/maps/@${session.location.lat},${session.location.lon},15z`"
+            target="new",
+            )
+            span.icon
+              i.mdi.mdi-google-maps
           // img(:src="`https://maps.googleapis.com/maps/api/staticmap?size=64x64&zoom=8&center=${session.location.lat},${session.location.lon}&format=png&style=feature:road.highway%7Celement:geometry%7Cvisibility:simplified%7Ccolor:0xc280e9&style=feature:transit.line%7Cvisibility:simplified%7Ccolor:0xbababa&style=feature:road.highway%7Celement:labels.text.stroke%7Cvisibility:on%7Ccolor:0xb06eba&style=feature:road.highway%7Celement:labels.text.fill%7Cvisibility:on%7Ccolor:0xffffff&key=${GOOGLE_API_KEY}`")
         td
           span.has-text-success(disabled,v-if="session.current") This Device
