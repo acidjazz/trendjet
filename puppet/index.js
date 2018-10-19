d675dd1f4860fda6040bca40e9d65d2cc60e93cc
kevin olson <acidjazz@gmail.com>
2018-10-19 01:45:57 -0500

    ✨ more progress, looks like the puppet is winning

--- puppet/index.js
+++ puppet/index.js
@@ -9,19 +9,20 @@ const fs = require('fs');
 const puppeteer = require('puppeteer');
 const useragent = require('user-agents');
 
-// const agent = new useragent().toString();
-let mobile = false;
-const agent = new useragent({ deviceCategory: 'mobile'}).toString();
-// $const agent = new useragent({ deviceCategory: 'desktop'}).toString();
+const agent = new useragent({ deviceCategory: 'desktop'}).toString();
 const before = new Date().getTime();
+
+let mobile = false;
 let el = '.ytp-large-play-button';
 
 (async() => {
+
   const browser = await puppeteer.launch();
-  // const browser = await puppeteer.launch({executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'});
   const page = await browser.newPage();
+
   page.setUserAgent(agent);
   await page.goto('https://www.youtube.com/watch?v=-LcV8duImEw');
+
   if (page._target._targetInfo.url.includes('https://m.youtube')) {
     mobile = true
   }
@@ -31,15 +32,13 @@ let el = '.ytp-large-play-button';
   }
 
   // fs.writeFileSync('body.html', await page.evaluate(() => document.body.innerHTML));
-  await page.waitFor(2000);
-
+  await page.waitFor(1000);
   await page.waitForSelector(el);
   await page.click(el);
-  await page.waitFor(12000);
-
-  await page.screenshot({path: `example-${new Date().getTime()}.png`});
-
+  await page.waitFor(2000);
+  await page.screenshot({type: 'jpeg', quality: 1, path: `example-${new Date().getTime()}.jpg`});
   await browser.close();
 
   console.log(`executed in ${((new Date().getTime() - before) / 1000)} seconds`);
+
 })();
