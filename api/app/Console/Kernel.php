479f13d47a12e0d6dfee4e88e7ef9308682d18ad
kevin olson <acidjazz@gmail.com>
2018-11-10 20:15:39 -0600

    🔧 forgot app

--- api/app/Console/Kernel.php
+++ api/app/Console/Kernel.php
@@ -5,7 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-use Console\Commands\PuppetCheck;
+use App\Console\Commands\PuppetCheck;
 
 class Kernel extends ConsoleKernel
 {
