6932c7b92e810d35c97336e2617a8e587d899e36
kevin olson <acidjazz@gmail.com>
2018-11-16 23:55:32 -0600

    🔧 model duration fillable

--- api/app/Models/Shot.php
+++ api/app/Models/Shot.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Shot extends Model
 {
-    protected $fillable = ['video_id', 'boost_id', 'file'];
+    protected $fillable = ['video_id', 'boost_id', 'file', 'duration'];
     protected $appends = ['url'];
 
     protected function getUrlAttribute()
