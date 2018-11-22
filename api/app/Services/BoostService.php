5c7816437da774dfeda4bc4c185d0cc4a1116543
kevin olson <acidjazz@gmail.com>
2018-11-21 19:02:15 -0600

    🔧 longer expiration period

--- api/app/Services/BoostService.php
+++ api/app/Services/BoostService.php
@@ -88,7 +88,7 @@ class BoostService {
             return [];
         }
         foreach ($instances as $instance) {
-            if ($instance['Runtime'] > 320) {
+            if ($instance['Runtime'] > 860) {
                 $ids[] = $instance['InstanceId'];
             }
         }
